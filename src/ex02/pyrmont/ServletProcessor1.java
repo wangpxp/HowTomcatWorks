950282f5786e84dd0a94e569a01dc1a2512b5ae9
wangpxp <wangpxp@gmail.com>
2020-01-10 22:41:12 +0800

    简单的servlet容器

---
+++ src/ex02/pyrmont/ServletProcessor1.java
@@ -0,0 +1,9 @@
+package ex02.pyrmont;
+
+import ex01.pyrmont.Request;
+import ex01.pyrmont.Response;
+
+public class ServletProcessor1 {
+    public void process(Request request, Response response) {
+    }
+}
---
+++ src/ex02/pyrmont/StaticResourceProcessor.java
@@ -0,0 +1,9 @@
+package ex02.pyrmont;
+
+import ex01.pyrmont.Request;
+import ex01.pyrmont.Response;
+
+public class StaticResourceProcessor {
+    public void process(Request request, Response response) {
+    }
+}
